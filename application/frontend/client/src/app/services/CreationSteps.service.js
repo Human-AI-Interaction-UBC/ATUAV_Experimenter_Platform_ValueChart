ec49427afbf3f7b0bceb837b8564c7529377f02f
Tiffany <tiffwu1027@gmail.com>
2021-02-07 18:45:39 -0800

    ran tsc to fix ts change

--- application/frontend/client/src/app/services/CreationSteps.service.js
+++ application/frontend/client/src/app/services/CreationSteps.service.js
@@ -267,13 +267,11 @@ let CreationStepsService = /** @class */ (() => {
                     status.chartId = this.valueChartService.getValueChart()._id;
                     this.valueChartHttp.setValueChartStatus(status).subscribe((newStatus) => { status = newStatus; });
                 }
-            }
-            // ,
-            // // Handle Server Errors
-            // (error) => {
-            // 	this.userNotificationService.displayWarnings(['Auto-saving failed']);
-            // }
-            );
+            }, 
+            // Handle Server Errors
+            (error) => {
+                this.userNotificationService.displayWarnings(['Auto-saving failed']);
+            });
         }
         /*
             @returns {void}
